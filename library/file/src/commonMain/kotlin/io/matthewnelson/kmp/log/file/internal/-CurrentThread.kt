4fc991ce801ad7b6d8e5d54951c49adfc17a66b2
Matthew Nelson <developer@matthewnelson.io>
2026-01-13 07:59:16 -0500

    Add thread utilities to inhibit Jvm InterruptibleChannel closure due to Thread.interrupted

---
+++ library/file/src/commonMain/kotlin/io/matthewnelson/kmp/log/file/internal/-CurrentThread.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2026 Matthew Nelson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+@file:Suppress("LEAKED_IN_PLACE_LAMBDA", "NOTHING_TO_INLINE", "WRONG_INVOCATION_KIND")
+
+package io.matthewnelson.kmp.log.file.internal
+
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
+
+internal object CurrentThread
+
+@OptIn(ExperimentalContracts::class)
+internal inline fun <T> CurrentThread.uninterrupted(block: () -> T): T {
+    contract { callsInPlace(block, InvocationKind.EXACTLY_ONCE) }
+    return uninterruptedImpl(block)
+}
+
+// TODO: CurrentThread.id(): String
+
+internal expect inline fun <T> CurrentThread.uninterruptedImpl(block: () -> T): T
