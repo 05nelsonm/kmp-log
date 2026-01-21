b2dd30d4c26d60130a2b1843bbf9d8f1274904aa
Matthew Nelson <developer@matthewnelson.io>
2026-01-21 14:22:49 -0500

    Use value class for domain separation to wrap `CoroutineScope` of `LogLoop` (#74)

---
+++ library/file/src/commonMain/kotlin/io/matthewnelson/kmp/log/file/internal/-LogLoopScope.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2026 Matthew Nelson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+package io.matthewnelson.kmp.log.file.internal
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.coroutineScope
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
+import kotlin.jvm.JvmInline
+
+@JvmInline
+internal value class LogLoopScope private constructor(internal val scope: CoroutineScope) {
+    internal companion object {
+        @OptIn(ExperimentalContracts::class)
+        internal suspend inline fun <R> logLoopScope(
+            crossinline block: suspend LogLoopScope.() -> R,
+        ): R {
+            contract { callsInPlace(block, InvocationKind.EXACTLY_ONCE) }
+            return coroutineScope { block(LogLoopScope(this)) }
+        }
+    }
+}
